f5fa7dfdf37bf0aeceaa1aa13990c72075a3f627
dimka <dimka@DESKTOP-9RENU9M>
2016-07-02 07:51:15 -0700

    Add project files.

---
+++ PlaceSharer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlaceSharer.Startup))]
+namespace PlaceSharer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
